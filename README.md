01a518decf0bbca58840491a9a624d9ed2265e28
simomiettinen <113496914+simomiettinen@users.noreply.github.com>
2022-12-07 16:09:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-tehtävä 6 käyttöliittymä hommat. /src/components pystyy vain lisäämään ja tarkastelemaan
+
---
+++ server.js
@@ -0,0 +1,53 @@
+require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
+
+const express = require("express");
+const app = express();
+
+/*CORS isn’t enabled on the server, this is due to security reasons by default,
+so no one else but the webserver itself can make requests to the server.*/
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  res.setHeader("Content-type", "application/json");
+
+  // Pass to next layer of middleware
+  next();
+});
+// Middleware
+app.use(express.json()); // parse json bodies in the request object
+
+// Redirect requests to endpoint starting with /posts to postRoutes.js
+app.use("/posts", require("./routes/postRoutes"));
+
+// Global Error Handler. IMPORTANT function params MUST start with err
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  console.log(err.name);
+  console.log(err.code);
+
+  res.status(500).json({
+    message: "Something went really wrong",
+  });
+});
+
+// Listen on pc port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
